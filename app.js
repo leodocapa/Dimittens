a39ac5c74d9e161e2f95b21a7c8fcb461523c651
Your Name <you@example.com>
2024-06-10 10:57:32 -0300

    leonardo

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-var teste
+const form = document.getElementById('form')
+const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const senha = document.getElementById('senha')
